11e93f4de1e53db97e8b42d4c79b409ac600727a
xialc <xialc@rayfaydata.com>
2024-03-05 19:20:27 +0800

    add split_file func

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    let mut args = args.clone();
+    args.reverse();
+    let func = args.pop();
+    let func = args.pop();
+    dbg!(&func);
+    match func {
+        Some(func) => {
+            if "split".eq(&func) {
+                split_file(args);
+            }
+        }
+        None => {
+            println!("Least 2 arguments are needed.")
+        }
+    }
+}
+
+fn split_file(args: Vec<String>) {
+    dbg!(args);
 }
