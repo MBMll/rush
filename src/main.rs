8191c1ee614e6a4e5770720e56e7d7aa4e5f00d9
xialc <xialc@rayfaydata.com>
2024-03-05 19:43:07 +0800

    fix eq

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
-use std::env;
+use std::{env, process::exit};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(&args);
+    // dbg!(&args);
     let mut args = args.clone();
     args.reverse();
+    args.pop();
     let func = args.pop();
-    let func = args.pop();
-    dbg!(&func);
+    // dbg!(&func);
     match func {
         Some(func) => {
             if "split".eq(&func) {
-                split_file(args);
+                return split_file(args);
             }
+            println!("Not found command: [{}]", func);
+            exit(404)
         }
         None => {
             println!("Least 2 arguments are needed.")
